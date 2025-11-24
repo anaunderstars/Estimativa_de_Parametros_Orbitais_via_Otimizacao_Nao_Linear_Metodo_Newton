2304c8d5502288f1a42e74048d26cb8054b02487
Ana HhHhH <84682693+anaunderstars@users.noreply.github.com>
2025-11-24 14:13:53 -0300

    Update calcular_gradiente.py

--- calcular_gradiente.py
+++ calcular_gradiente.py
@@ -12,3 +12,4 @@ def calcular_gradiente (theta,t_obs,r_obs,mu,h =1e-8) :
         J_perturbed = funcao_objetivo (theta_perturbed,t_obs,r_obs,mu)
         grad[i] = (J_perturbed - J0 )/h
     return grad
+    
